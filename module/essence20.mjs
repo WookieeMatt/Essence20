b903c8b2b42bec926f10d78edc725f8d87778b83
Phil Dominguez <phild23@gmail.com>
2022-04-19 08:25:02 -0400

    Making initiative shifts start with d20 like the others

--- module/essence20.mjs
+++ module/essence20.mjs
@@ -30,7 +30,7 @@ Hooks.once('init', async function() {
    * @type {String}
    */
   CONFIG.Combat.initiative = {
-    formula: "1d20 + @initiative",
+    formula: "@initiativeFormula",
   };
 
   // Define custom Document classes
