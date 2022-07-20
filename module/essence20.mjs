5d50fad43c6451bbf9175ee899a7098b3c68335a
Phil Dominguez <phild23@gmail.com>
2022-07-20 13:48:17 -0400

    Adding ifEquals helper; Only showing mod/isSpec for NPCs

--- module/essence20.mjs
+++ module/essence20.mjs
@@ -84,6 +84,10 @@ Handlebars.registerHelper('isdefined', function (value) {
   return value !== undefined;
 });
 
+Handlebars.registerHelper('ifEquals', function(arg1, arg2, options) {
+  return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
+});
+
 /* -------------------------------------------- */
 /*  Ready Hook                                  */
 /* -------------------------------------------- */
