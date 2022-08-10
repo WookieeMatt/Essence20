5dd51d6976b0b8ed5295e8aca397eb0290bd479f
Phil Dominguez <phild23@gmail.com>
2022-08-10 10:04:13 -0400

    Moving PC skills to partial

--- module/helpers/templates.mjs
+++ module/helpers/templates.mjs
@@ -16,6 +16,7 @@ export const preloadHandlebarsTemplates = async function () {
     "systems/essence20/templates/actor/parts/actor-accordion-skills.hbs",
     "systems/essence20/templates/actor/parts/actor-armor.hbs",
     "systems/essence20/templates/actor/parts/actor-background.hbs",
+    "systems/essence20/templates/actor/parts/actor-pc-skills.hbs",
     "systems/essence20/templates/actor/parts/actor-collapsible-item-container.hbs",
     "systems/essence20/templates/actor/parts/actor-common.hbs",
     "systems/essence20/templates/actor/parts/actor-defenses.hbs",
