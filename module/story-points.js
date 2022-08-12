5c42d1b69b0dbddc74b9d85904873af6ae12515d
Phil Dominguez <phild23@gmail.com>
2022-08-11 20:50:15 -0400

    SP buttons and display working

--- module/story-points.js
+++ module/story-points.js
@@ -1,20 +1,15 @@
 import { registerSettings } from "./settings.js";
 
 export let i18n = key => {
-    return game.i18n.localize(key);
+  return game.i18n.localize(key);
 };
 export let setting = key => {
-    return game.settings.get("essence20", key);
+  return game.settings.get("essence20", key);
 };
 
 export class StoryPoints extends Application {
-  tokenname = '';
-  tokenstat = '';
-  tokentemp = '';
-  tokentooltip = '';
-  color = "";
-  valuePct = null;
-  tempPct = null;
+  gmPoints = 0;
+  storyPoints = 0;
 
   static get defaultOptions() {
     let pos = null;
@@ -26,7 +21,34 @@ export class StoryPoints extends Application {
       resizable: false,
       top: pos?.top || 60,
       left: pos?.left || (($('#board').width / 2) - 150),
-      width: 300,
+      storyPoints: 0,
+    });
+  }
+
+  getData() {
+    return {
+        storyPoints: this.storyPoints,
+    };
+  }
+
+  changePoints(value) {
+    this.storyPoints = value;
+    console.log(this.element)
+    $('#storypoints-input', this.element).val(value);
+  }
+
+  activateListeners(html) {
+    super.activateListeners(html);
+
+    html.find('#storypoints-btn-hurt').click(ev => {
+        ev.preventDefault();
+        console.log('set character to hurt');
+        this.changePoints(Math.max(0, this.storyPoints - 1));
+    });
+    html.find('#storypoints-btn-heal').click(ev => {
+        ev.preventDefault();
+        console.log('set character to heal');
+        this.changePoints(this.storyPoints + 1);
     });
   }
 }
