9f15d9bc47a105fcb914d9176a48521b51d1d159
Phil Dominguez <phild23@gmail.com>
2022-08-11 21:33:10 -0400

    Message sent to chat when spending story points

--- module/story-points.js
+++ module/story-points.js
@@ -1,4 +1,7 @@
 import { registerSettings } from "./settings.js";
+export let i18n = key => {
+  return game.i18n.localize(key);
+};
 
 export class StoryPoints extends Application {
   gmPoints = game.settings.get('essence20', 'gm-points');
@@ -18,6 +21,7 @@ export class StoryPoints extends Application {
     });
   }
 
+  // Data to be access within the template
   getData() {
     return {
       gmPoints: this.gmPoints,
@@ -25,12 +29,14 @@ export class StoryPoints extends Application {
     };
   }
 
+  // Handles changing GM Points to given value
   changeGmPoints(value) {
     this.gmPoints = value;
     $('#gmpoints-input', this.element).val(value);
     game.settings.set('essence20', 'gm-points', value);
   }
 
+  // Handles changing Story Points to given value
   changeStoryPoints(value) {
     this.storyPoints = value;
     $('#storypoints-input', this.element).val(value);
@@ -40,24 +46,41 @@ export class StoryPoints extends Application {
   activateListeners(html) {
     super.activateListeners(html);
 
+    // GM Point clicks
     html.find('#gmpoints-btn-hurt').click(ev => {
       ev.preventDefault();
       this.changeGmPoints(Math.max(0, this.gmPoints - 1));
+      this.sendMessage(`${i18n("E20.STORY_POINTS.gmPointSpent")}`);
     });
     html.find('#gmpoints-btn-heal').click(ev => {
       ev.preventDefault();
       this.changeGmPoints(this.gmPoints + 1);
     });
 
-
+    // Story Point clicks
     html.find('#storypoints-btn-hurt').click(ev => {
       ev.preventDefault();
       this.changeStoryPoints(Math.max(0, this.storyPoints - 1));
+      this.sendMessage(`${i18n("E20.STORY_POINTS.storyPointSpent")}`);
     });
     html.find('#storypoints-btn-heal').click(ev => {
       ev.preventDefault();
       this.changeStoryPoints(this.storyPoints + 1);
     });
+  }
+
+  // Outputs given message to chat
+  sendMessage(content) {
+    const speaker = ChatMessage.getSpeaker({ user: game.user.id });
+
+    let messageData = {
+      user: game.user.id,
+      speaker: speaker,
+      type: CONST.CHAT_MESSAGE_TYPES.OTHER,
+      content,
+    };
+
+    ChatMessage.create(messageData);
   }
 }
 
