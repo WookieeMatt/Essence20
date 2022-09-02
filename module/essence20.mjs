755bf683e4444c593baeb751259221cad150d78c
Phil Dominguez <phild23@gmail.com>
2022-09-02 19:03:16 -0400

    Fixing hotbar drop behavior

--- module/essence20.mjs
+++ module/essence20.mjs
@@ -94,7 +94,12 @@ Handlebars.registerHelper('ifEquals', function (arg1, arg2, options) {
 
 Hooks.once("ready", async function () {
   // Wait to register hotbar drop hook on ready so that modules could register earlier if they want to
-  Hooks.on("hotbarDrop", (bar, data, slot) => createItemMacro(data, slot));
+  Hooks.on("hotbarDrop", (bar, data, slot) => {
+    if (["Item", "ActiveEffect"].includes(data.type)) {
+      createItemMacro(data, slot);
+      return false;
+    }
+  });
 });
 
 /* -------------------------------------------- */
