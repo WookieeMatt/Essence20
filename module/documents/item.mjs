c93365b2a81304049a55f28e92d18f2799a46e62
Phil Dominguez <phild23@gmail.com>
2023-02-19 20:00:50 -0500

    Converting trait

--- module/documents/item.mjs
+++ module/documents/item.mjs
@@ -75,7 +75,7 @@ export class Essence20Item extends Item {
         flavor: label,
         content: content,
       });
-    } else if (['altMode', 'armor', 'classFeature', 'gear', 'hangUp'].includes(this.type)) {
+    } else if (['altMode', 'armor', 'classFeature', 'gear', 'hangUp', 'trait'].includes(this.type)) {
       // Initialize chat data.
       const speaker = ChatMessage.getSpeaker({ actor: this.actor });
       const rollMode = game.settings.get('core', 'rollMode');
