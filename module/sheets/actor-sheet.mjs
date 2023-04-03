9a0d9db1dc1f134083b3e66afea018222bfb20b7
JFranklin3000 <talinthalas@msn.com>
2023-04-02 20:11:47 -0500

    fixed NPC skills with ranks.

--- module/sheets/actor-sheet.mjs
+++ module/sheets/actor-sheet.mjs
@@ -87,11 +87,9 @@ export class Essence20ActorSheet extends ActorSheet {
     }
 
     // Include any skills not d20, are specialized, or have a modifier
-    for (let [_, skills] of Object.entries(context.system.skills)) {
-      for (let [skill, fields] of Object.entries(skills)) {
-        if (fields.shift != 'd20' || fields.isSpecialized || fields.modifier) {
-          displayedNpcSkills[skill] = true;
-        }
+    for (let [skill, fields] of Object.entries(context.system.skills)) {
+      if (fields.shift != 'd20' || fields.isSpecialized || fields.modifier) {
+        displayedNpcSkills[skill] = true;
       }
     }
 
