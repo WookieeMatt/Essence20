fe90a82a340f441f554cd82109142ddc5fe45d01
Phil Dominguez <phild23@gmail.com>
2023-05-05 19:58:38 -0400

    Refactored to use switch

--- module/sheets/actor-sheet.mjs
+++ module/sheets/actor-sheet.mjs
@@ -431,20 +431,24 @@ export class Essence20ActorSheet extends ActorSheet {
     const sourceItem = await fromUuid(data.uuid);
     if (!sourceItem) return false;
 
-    if (sourceItem.type == 'origin') {
-      for (let actorItem of this.actor.items) {
-        if(actorItem.type == 'origin') {
-          ui.notifications.error(game.i18n.format(game.i18n.localize('E20.MulitpleOriginError')));
-          return false
+    switch (sourceItem.type) {
+      case 'origin':
+        for (let actorItem of this.actor.items) {
+          if(actorItem.type == 'origin') {
+            ui.notifications.error(game.i18n.format(game.i18n.localize('E20.MulitpleOriginError')));
+            return false;
+          }
         }
-      }
 
-      await this._showOriginEssenceDialog(sourceItem, event, data);
-    } else if (this.actor.type == 'companion' && this.actor.system.type == 'drone' && sourceItem.type == 'upgrade' && sourceItem.system.type != 'drone') {
-      // Drones can only accept drone upgrades
-      return;
-    } else {
-      super._onDropItem(event, data);
+        await this._showOriginEssenceDialog(sourceItem, event, data);
+        break;
+      case 'upgrade':
+        // Drones can only accept drone upgrades
+        if (this.actor.type == 'companion' && this.actor.system.type == 'drone' && sourceItem.system.type != 'drone') {
+          return false;
+        }
+      default:
+        super._onDropItem(event, data);
     }
   };
 
