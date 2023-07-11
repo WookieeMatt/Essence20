7a448098a185edb998e7b837281a3e3f57f1297c
JFranklin3000 <71140111+JFranklin3000@users.noreply.github.com>
2023-07-11 07:58:58 -0500

    Update module/sheet-handlers/alteration-handler.mjs
    Co-authored-by: phildominguez <41161497+phildominguez@users.noreply.github.com>

--- module/sheet-handlers/alteration-handler.mjs
+++ module/sheet-handlers/alteration-handler.mjs
@@ -210,9 +210,9 @@ export class AlterationHandler {
   /**
   * Handles the setting of values from a essence bonus alteration
   * @param {Alteration} alteration The alteration
-  * @param {Text} bonusSkill    The skill selected from _processAlterationSkillIncrease()
+  * @param {String} bonusSkill    The skill selected from _processAlterationSkillIncrease()
   * @param {Function} dropFunc   The function to call to complete the Alteration drop
-  * @param {Text} costEssence    The essence selected from _showAlterationCostEssenceDialog()
+  * @param {String} costEssence    The essence selected from _showAlterationCostEssenceDialog()
   * @param {Object} options    The options resulting from _showAlterationCostSkillDialog()
   */
   async _alterationStatUpdate(alteration, bonusSkill, dropFunc, costEssence, options) {
