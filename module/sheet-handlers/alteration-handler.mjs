b4c8da3891614da3c507a4fc3e18da6ddacac020
JFranklin3000 <71140111+JFranklin3000@users.noreply.github.com>
2023-09-17 15:39:29 -0500

    Update module/sheet-handlers/alteration-handler.mjs
    Co-authored-by: phildominguez <41161497+phildominguez@users.noreply.github.com>

--- module/sheet-handlers/alteration-handler.mjs
+++ module/sheet-handlers/alteration-handler.mjs
@@ -196,7 +196,7 @@ export class AlterationHandler {
   * Handles choosing a skill to increase
   * @param {Alteration} alteration The alteration
   * @param {Object} options    The options resulting from _showAlterationBonusSkillDialog()
-  * @param {AlterationUUID} alterationUuid The original ID of the alteration
+  * @param {String} alterationUuid The original ID of the alteration
   * @param {Function} dropFunc   The function to call to complete the Alteration drop
   */
   async _processAlterationSkillIncrease(alteration, options, alterationUuid, dropFunc) {
