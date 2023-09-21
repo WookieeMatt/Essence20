6fffc4b5fece27b804be258dbecb180e2f64f381
JFranklin3000 <talinthalas@msn.com>
2023-09-21 15:15:41 -0500

    Assets Icon restructuring

--- module/helpers/config.mjs
+++ module/helpers/config.mjs
@@ -513,72 +513,72 @@ preLocalize("pointsNameOptions");
 
 E20.statusEffects = [
   {
-    icon: 'systems/essence20/assets/icons/status_asleep.svg',
+    icon: 'systems/essence20/assets/icons/status_effects/status_asleep.svg',
     id: 'asleep',
     label: 'E20.StatusAsleep',
   },
   {
-    icon: 'systems/essence20/assets/icons/status_blinded.svg',
+    icon: 'systems/essence20/assets/icons/status_effects/status_blinded.svg',
     id: 'blinded',
     label: 'E20.StatusBlinded',
   },
   {
-    icon: 'systems/essence20/assets/icons/status_deafened.svg',
+    icon: 'systems/essence20/assets/icons/status_effects/status_deafened.svg',
     id: 'deafened',
     label: 'E20.StatusDeafened',
   },
   {
-    icon: 'systems/essence20/assets/icons/status_defeated.svg',
+    icon: 'systems/essence20/assets/icons/status_effects/status_defeated.svg',
     id: 'defeated',
     label: 'E20.StatusDefeated',
   },
   {
-    icon: 'systems/essence20/assets/icons/status_frightened.svg',
+    icon: 'systems/essence20/assets/icons/status_effects/status_frightened.svg',
     id: 'frightened',
     label: 'E20.StatusFrightened',
   },
   {
-    icon: 'systems/essence20/assets/icons/status_grappled.svg',
+    icon: 'systems/essence20/assets/icons/status_effects/status_grappled.svg',
     id: 'grappled',
     label: 'E20.StatusGrappled',
   },
   {
-    icon: 'systems/essence20/assets/icons/status_immobilized.svg',
+    icon: 'systems/essence20/assets/icons/status_effects/status_immobilized.svg',
     id: 'immobilized',
     label: 'E20.StatusImmobilized',
   },
   {
-    icon: 'systems/essence20/assets/icons/status_impaired.svg',
+    icon: 'systems/essence20/assets/icons/status_effects/status_impaired.svg',
     id: 'Impaired',
     label: 'E20.StatusImpaired',
   },
   {
-    icon: 'systems/essence20/assets/icons/status_invisible.svg',
+    icon: 'systems/essence20/assets/icons/status_effects/status_invisible.svg',
     id: 'invisible',
     label: 'E20.StatusInvisible',
   },
   {
-    icon: 'systems/essence20/assets/icons/status_mesmerized.svg',
+    icon: 'systems/essence20/assets/icons/status_effects/status_mesmerized.svg',
     id: 'mesmerized',
     label: 'E20.StatusMesmerized',
   },
   {
-    icon: 'systems/essence20/assets/icons/status_prone.svg',
+    icon: 'systems/essence20/assets/icons/status_effects/status_prone.svg',
     id: 'prone',
     label: 'E20.StatusProne',
   },
   {
-    icon: 'systems/essence20/assets/icons/status_restrained.svg',
+    icon: 'systems/essence20/assets/icons/status_effects/status_restrained.svg',
     id: 'restrained',
     label: 'E20.StatusRestrained',
   },
   {
-    icon: 'systems/essence20/assets/icons/status_stunned.svg',
+    icon: 'systems/essence20/assets/icons/status_effects/status_stunned.svg',
     id: 'stunned',
     label: 'E20.StatusStunned',
   },
   {
-    icon: 'systems/essence20/assets/icons/status_unconscious.svg',
+    icon: 'systems/essence20/assets/icons/status_effects/status_unconscious.svg',
     id: 'unconscious',
     label: 'E20.StatusUnconscious',
   },
