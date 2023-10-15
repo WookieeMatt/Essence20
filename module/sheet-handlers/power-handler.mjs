66e4874f78f4f8ffccc8cb2aafde0caa3ffdee9b
JFranklin3000 <71140111+JFranklin3000@users.noreply.github.com>
2023-10-15 12:11:45 -0500

    Powers drop (#527)
    In this PR
    Added drop functionality
    Removed the drop down and hid the roll for power without activations
    Testing
    create powers and drop on to actors
    make sure activation works and max number of powers as well.

--- module/sheet-handlers/power-handler.mjs
+++ module/sheet-handlers/power-handler.mjs
@@ -1,3 +1,7 @@
+import {
+  parseId,
+} from "../helpers/utils.mjs";
+
 export class PowerHandler {
   /**
   * Constructor
@@ -14,8 +18,30 @@ export class PowerHandler {
   * @param {Function} dropFunc   The function to call to complete the Power drop
   */
   async powerUpdate(power, dropFunc) {
+    const powerUuid = parseId(power.uuid);
+    let timesTaken = 0;
+    let classFeatureId = "";
+
+    for (let actorItem of this._actor.items) {
+      if (actorItem.type =='classFeature') {
+        if (power.system.type == "grid" && actorItem.name == 'Personal Power') {
+          classFeatureId = actorItem._id;
+        }
+      } else if (actorItem.type == 'power' && actorItem.system.originalId == powerUuid) {
+        timesTaken++;
+        if (power.system.selectionLimit == timesTaken) {
+          ui.notifications.error(game.i18n.localize('E20.PowerAlreadyTaken'));
+          return;
+        }
+      }
+    }
+
     const newPowerList = await dropFunc();
     const newPower = newPowerList[0];
-    console.log(power, newPower);
+
+    await newPower.update ({
+      "system.originalId": powerUuid,
+      "system.classFeatureId": classFeatureId,
+    });
   }
 }
