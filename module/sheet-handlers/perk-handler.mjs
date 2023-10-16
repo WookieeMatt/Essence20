f23feb3f65fcaead24bbc8c1b67960617f599af8
JFranklin3000 <71140111+JFranklin3000@users.noreply.github.com>
2023-10-15 20:01:51 -0500

    Sorcerous powers (#528)
    In this PR
    Add the Finsters and Furious Fighters Compendiums
    Created the Sorcery General Perk
    Created Sorcerous Power as a ClassFeature that gets added when the Perk is added.
    Testing
    build fmmcitems pack
    drop item on to Power Ranger
    create power and validate Sorcerous Power is an option in the drop down.

---
+++ module/sheet-handlers/perk-handler.mjs
@@ -0,0 +1,43 @@
+import {
+  parseId,
+} from "../helpers/utils.mjs";
+
+const SORCERY_PERK_ID = "xUBOE1s5pgVyUrwj";
+
+export class PerkHandler {
+  /**
+  * Constructor
+  * @param {Essence20ActorSheet} actorSheet The actor sheet
+  */
+  constructor(actorSheet) {
+    this._actorSheet = actorSheet;
+    this._actor = actorSheet.actor;
+  }
+
+  /**
+  * Handle the dropping of a power on to a character
+  * @param {Perk} perk The perk
+  * @param {Function} dropFunc   The function to call to complete the Power drop
+  */
+  async perkUpdate(perk, dropFunc) {
+    const perkUuid = parseId(perk.uuid);
+
+    if (perkUuid == SORCERY_PERK_ID) {
+      Item.create(
+        {
+          name: game.i18n.localize('E20.SorcerousPower'),
+          type: 'classFeature',
+          data: {},
+        },
+        { parent: this._actor},
+      );
+    }
+
+    const newPerkList = await dropFunc();
+    const newPerk = newPerkList[0];
+
+    await newPerk.update ({
+      "system.originalId": perkUuid,
+    });
+  }
+}
