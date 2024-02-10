982954f3ab164019a4c8a431804305e6a254284b
Phil Dominguez <phild23@gmail.com>
2024-02-10 18:46:01 -0500

    Adding character

---
+++ module/data/actor/character.mjs
@@ -0,0 +1,42 @@
+import { E20 } from "../../helpers/config.mjs";
+import { makeInt } from "../generic-makers.mjs";
+
+export function makeDefensesFields(name, essence) {
+  return new fields.SchemaField({
+    toughness: new fields.SchemaField({
+      armor: makeInt(),
+      base: makeInt(10),
+      bonus: makeInt(),
+      essence: new fields.StringField({initial: essence}),
+      morphed: makeInt(),
+      name: new fields.StringField({initial: name}),
+    }),
+  });
+}
+
+class CharacterActorData extends foundry.abstract.DataModel {
+  static defineSchema() {
+    const fields = foundry.data.fields;
+    return {
+      background: new fields.SchemaField({
+        pronouns: new fields.StringField({initial: ''}),
+        role: new fields.StringField({initial: ''}),
+      }),
+      defenses: new fields.SchemaField({
+        toughness: makeDefensesFields(E20.essences.toughness, E20.essences.strength),
+        evasion: makeDefensesFields(E20.essences.evasion, E20.essences.speed),
+        willpower: makeDefensesFields(E20.essences.willpower, E20.essences.smarts),
+        cleverness: makeDefensesFields(E20.essences.cleverness, E20.essences.social),
+      }),
+      essences: new fields.SchemaField({
+        strength: makeInt(3),
+        speed: makeInt(3),
+        smarts: makeInt(3),
+        social: makeInt(3),
+      }),
+      influenceIds: new fields.ArrayField(new fields.StringField()), // Idt we need this
+      level: makeInt(1),
+      notes: new fields.HTMLField(),
+    };
+  }
+}
