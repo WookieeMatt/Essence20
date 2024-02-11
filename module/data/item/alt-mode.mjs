c4edbf29023056776699579c971ac94b13791a11
Phil Dominguez <phild23@gmail.com>
2024-02-11 10:05:41 -0500

    Adding alt-mode

---
+++ module/data/item/alt-mode.mjs
@@ -0,0 +1,25 @@
+import { item } from './item';
+import { itemDescription } from './item-description';
+import { makeInt } from "../../generic-makers.mjs";
+
+const fields = foundry.data.fields;
+
+class AltModeItemData extends foundry.abstract.DataModel {
+  static defineSchema() {
+    return {
+      ...item(),
+      ...itemDescription(),
+      crew: makeInt(),
+      firepoints: makeInt(),
+      movement: fields.SchemaField({
+        aerial: makeInt(),
+        aquatic: makeInt(),
+        ground: makeInt(),
+      }),
+      size: new fields.StringField({
+        initial: 'common',
+        choices: Object.values(E20.actorSizes),
+      }),
+    };
+  }
+}
