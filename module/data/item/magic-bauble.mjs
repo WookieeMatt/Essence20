21d11b206f4c7066bcf14ee6cb0e0bb8ec2f9b5e
Phil Dominguez <phild23@gmail.com>
2024-02-11 10:47:37 -0500

    Adding magic-bauble

---
+++ module/data/item/magic-bauble.mjs
@@ -0,0 +1,17 @@
+import { item } from './item';
+import { itemDescription } from './item-description';
+
+class MagicBaubleItemData extends foundry.abstract.DataModel {
+  static defineSchema() {
+    return {
+      ...item(),
+      ...itemDescription(),
+      duration: new fields.StringField({initial: ''}),
+      range: new fields.StringField({initial: ''}),
+      spellcastingShift: new fields.StringField({
+        choices: Object.values(E20.skillRollableShifts),
+        initial: 'd20',
+      }),
+    };
+  }
+}
