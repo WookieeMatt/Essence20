506c5e9f196916dd5c2a3e741320d2935ee89022
Phil Dominguez <phild23@gmail.com>
2024-02-10 23:08:57 -0500

    Adding power-ranger

---
+++ module/data/actor/power-ranger.mjs
@@ -0,0 +1,16 @@
+import {common} from './common';
+import {creature} from './creature';
+import {character} from './character';
+
+const fields = foundry.data.fields;
+
+class PowerRangerActorData extends foundry.abstract.DataModel {
+  static defineSchema() {
+    return {
+      ...character(),
+      ...common(),
+      ...creature(),
+      canMorph: new fields.BooleanField({initial: false}),
+    };
+  }
+}
