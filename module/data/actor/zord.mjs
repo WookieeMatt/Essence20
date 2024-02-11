e5167b29fb3ec6761c9141e118f9d5166ebfb0d0
Phil Dominguez <phild23@gmail.com>
2024-02-10 23:26:06 -0500

    Adding zord

---
+++ module/data/actor/zord.mjs
@@ -0,0 +1,19 @@
+import { common } from './common';
+import { machine } from './machine';
+import { zordBase } from './zordBase';
+import { makeInt } from "../generic-makers.mjs";
+
+const fields = foundry.data.fields;
+
+class ZordActorData extends foundry.abstract.DataModel {
+  static defineSchema() {
+    return {
+      ...common(),
+      ...machine(),
+      ...zordBase(),
+      isCombiner: new fields.BooleanField({initial: true}),
+      prerequisite: new fields.StringField({initial: ''}),
+      ranger: new fields.StringField({initial: ''}),
+    };
+  }
+}
