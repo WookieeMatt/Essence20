4b0a3abec21ee522dba29ada99f1b60c39aa3b67
JFranklin3000 <talinthalas@msn.com>
2024-02-16 17:39:34 -0600

    Update essence20.mjs

--- module/essence20.mjs
+++ module/essence20.mjs
@@ -172,9 +172,9 @@ Hooks.on("renderChatMessage", (app, html, data) => {
 Hooks.on("renderDialog", (dialog, html) => {
   if (html[0].innerText.includes('Create New Item')) {
     const select = html[0].querySelector("select[name='type']");
-    const disable = select.querySelector("option[value='threatPower']");
-    if (disable) {
-      disable.setAttribute('disabled', '');
+    const threatPowerOption = select.querySelector("option[value='threatPower']");
+    if (threatPowerOption) {
+      threatPowerOption.style.display = 'none';
     }
 
     if (select) {
