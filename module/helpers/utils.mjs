76096238d24e30c4c6dd42af1cceac63672f1844
JFranklin3000 <talinthalas@msn.com>
2024-03-16 10:59:58 -0500

    Update utils.mjs

--- module/helpers/utils.mjs
+++ module/helpers/utils.mjs
@@ -538,15 +538,14 @@ export async function setRoleValues(role, actor, newLevel=null, previousLevel=nu
     const skillStringIsSpecialized = `system.skills.${skillName}.isSpecialized`;
 
     let isSpecialized = false;
-    if (role.system.skillDie.specializedLevels) {
-      for (const arrayLevel of role.system.skillDie.specializedLevels) {
-        const level = arrayLevel.replace(/[^0-9]/g, '');
-        if (actor.system.level == level) {
-          isSpecialized = true;
-          break;
-        }
+    for (const arrayLevel of role.system.skillDie.specializedLevels) {
+      const level = arrayLevel.replace(/[^0-9]/g, '');
+      if (actor.system.level == level) {
+        isSpecialized = true;
+        break;
       }
     }
+
 
     await actor.update({
       [skillStringShift]: shiftList[finalShiftIndex],
