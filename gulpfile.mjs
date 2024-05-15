e5fafeb2d86d98f255bcc3cc5fd73fe582b756ed
Phil Dominguez <phild23@gmail.com>
2024-05-15 15:54:23 -0400

    Removing gulp sass stuff

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -2,65 +2,12 @@ import {compilePack, extractPack} from "@foundryvtt/foundryvtt-cli";
 import fs from "fs";
 import gulp from "gulp";
 import logger from "fancy-log";
-// const gulp = require('gulp');
-// const prefix = require('gulp-autoprefixer');
-// const sourcemaps = require('gulp-sourcemaps');
-// const sass = require('gulp-sass')(require('sass'));
 
 /**
  * Folder where source JSON files should be located relative to the system folder.
  * @type {string}
  */
 const PACK_SOURCE = "/_source/";
-
-/* ----------------------------------------- */
-/*  Compile Sass
-/* ----------------------------------------- */
-
-// // Small error handler helper function.
-// function handleError(err) {
-//   console.log(err.toString());
-//   this.emit('end');
-// }
-
-// const SYSTEM_SCSS = ["sass/*.scss"];
-// function compileScss() {
-//   // Configure options for sass output. For example, 'expanded' or 'nested'
-//   let options = {
-//     outputStyle: 'expanded'
-//   };
-//   return gulp.src(SYSTEM_SCSS)
-//     .pipe(
-//       sass(options)
-//         .on('error', handleError)
-//     )
-//     .pipe(prefix({
-//       cascade: false
-//     }))
-//     .pipe(gulp.dest("./css"))
-// }
-// const css = gulp.series(compileScss);
-
-// /* ----------------------------------------- */
-// /*  Watch Updates
-// /* ----------------------------------------- */
-
-// function watchUpdates() {
-//   gulp.watch(SYSTEM_SCSS, css);
-// }
-
-// /* ----------------------------------------- */
-// /*  Export Tasks
-// /* ----------------------------------------- */
-
-// exports.default = gulp.series(
-//   compileScss,
-//   watchUpdates
-// );
-// exports.build = gulp.series(
-//   compileScss
-// );
-// exports.css = css;
 
 /**
  * Compile the source JSON files into compendium packs.
