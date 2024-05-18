f2d41683cd9d1207d3e0f7f009a97d299f476213
Phil Dominguez <phild23@gmail.com>
2024-05-17 21:17:26 -0400

    Bunch of alteration fixes

--- module/data/item/alteration.mjs
+++ module/data/item/alteration.mjs
@@ -1,23 +1,33 @@
-import { makeStr } from "../generic-makers.mjs";
+import { E20 } from "../../helpers/config.mjs";
+
+import { makeStr, makeStrWithChoices } from "../generic-makers.mjs";
 
 import { item } from './templates/item.mjs';
 import { itemDescription } from './templates/item-description.mjs';
+
+const fields = foundry.data.fields;
 
 export class AlterationItemData extends foundry.abstract.TypeDataModel {
   static defineSchema() {
     return {
       ...item(),
       ...itemDescription(),
-      availability: makeStr('standard'),
+      availability: makeStrWithChoices('standard', Object.keys(E20.availabilities)),
       benefit: makeStr(''),
-      bonusSkill: makeStr(''),
-      cost: makeStr(''),
-      costSkill: makeStr(''),
+      bonus: makeStr(null),
+      bonusSkill: makeStrWithChoices(null, Object.keys(E20.skills)),
+      cost: makeStr(null),
+      costSkill: makeStrWithChoices(null, Object.keys(E20.skills)),
       essenceBenefit: makeStr(''),
-      essenceCost: makeStr(''),
+      essenceBonus: new fields.ArrayField(
+        makeStrWithChoices(null, Object.keys(E20.essences)),
+      ),
+      essenceCost: new fields.ArrayField(
+        makeStrWithChoices(null, Object.keys(E20.essences)),
+      ),
       movementCost: makeStr(''),
-      selectedEssence: makeStr(''),
-      type: makeStr('other'),
+      selectedEssence: makeStrWithChoices(null, Object.keys(E20.essences)),
+      type: makeStrWithChoices('other', Object.keys(E20.alterationTypes)),
     };
   }
 }
