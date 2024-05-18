b01cd5aa356798f6546e57b1cfb1d7452524c16d
Phil Dominguez <phild23@gmail.com>
2024-05-18 10:24:03 -0400

    Fixes for origin

--- module/data/item/origin.mjs
+++ module/data/item/origin.mjs
@@ -1,8 +1,12 @@
-import { makeInt, makeStr, makeStrArray } from "../generic-makers.mjs";
+import { E20 } from "../../helpers/config.mjs";
+
+import { makeInt, makeStr, makeStrWithChoices } from "../generic-makers.mjs";
 
 import { item } from './templates/item.mjs';
 import { itemDescription } from './templates/item-description.mjs';
 import { parentItem } from './templates/parent-item.mjs';
+
+const fields = foundry.data.fields;
 
 export class OriginItemData extends foundry.abstract.TypeDataModel {
   static defineSchema() {
@@ -13,9 +17,13 @@ export class OriginItemData extends foundry.abstract.TypeDataModel {
       baseAerialMovement: makeInt(0),
       baseAquaticMovement: makeInt(0),
       baseGroundMovement: makeInt(0),
-      essences: makeStrArray(),
+      essences: new fields.ArrayField(
+        makeStrWithChoices(Object.keys(E20.essences)),
+      ),
       languages: makeStr(''),
-      skills: makeStrArray(),
+      skills: new fields.ArrayField(
+        makeStrWithChoices(Object.keys(E20.originSkills)),
+      ),
       startingHealth: makeInt(0),
     };
   }
