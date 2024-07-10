710e513ac13711b290253fe6fd7098c4da274c9f
Phil Dominguez <phild23@gmail.com>
2024-07-10 19:25:01 -0400

    Adding awaits to role/focus updates

--- module/sheet-handlers/role-handler.mjs
+++ module/sheet-handlers/role-handler.mjs
@@ -368,14 +368,14 @@ export async function onLevelChange(actor, newLevel) {
 
   const roles = getItemsOfType("role", actor.items);
   if (roles.length == 1) {
-    setRoleValues(roles[0], actor, newLevel, previousLevel);
+    await setRoleValues(roles[0], actor, newLevel, previousLevel);
   } else {
     return;
   }
 
   const focus = getItemsOfType("focus", actor.items);
   if (focus.length == 1) {
-    setFocusValues(focus[0], actor, newLevel, previousLevel);
+    await setFocusValues(focus[0], actor, newLevel, previousLevel);
   }
 
   actor.setFlag('essence20', 'previousLevel', newLevel);
