b0e36fc548acdbe311a6a5e626c3d73c4271feed
JFranklin3000 <talinthalas@msn.com>
2024-08-19 19:34:32 -0500

    changes to support d2 criticals.

--- module/dice.mjs
+++ module/dice.mjs
@@ -147,7 +147,7 @@ export class Dice {
     if (E20.autoSuccessShifts.includes(finalShift)) {
       finalShift = E20.skillRollableShifts[E20.skillRollableShifts.length - 1];
     }
-
+    const canCritD2 = dataset.canCritD2 || skillRollOptions.canCritD2;
     const isSpecialized = dataset.isSpecialized || skillRollOptions.isSpecialized;
     const modifier = actorSkillData.modifier || 0;
     const formula = this._getFormula(isSpecialized, skillRollOptions, finalShift, modifier);
@@ -162,7 +162,7 @@ export class Dice {
         }) + '<br>';
       }
 
-      this._rollSkillHelper(formula, actor, repeatText + label);
+      this._rollSkillHelper(formula, actor, repeatText + label, canCritD2);
     }
   }
 
@@ -173,9 +173,12 @@ export class Dice {
    * @param {String} flavor   The html to use for the roll message.
    * @private
    */
-  _rollSkillHelper(formula, actor, flavor) {
+  _rollSkillHelper(formula, actor, flavor, canCritD2) {
     let roll = new Roll(formula, actor.getRollData());
     roll.toMessage({
+      flags: {
+        canCritD2: canCritD2,
+      },
       speaker: this._chatMessage.getSpeaker({ actor }),
       flavor,
       rollMode: game.settings.get('core', 'rollMode'),
