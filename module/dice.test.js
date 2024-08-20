f89e0b5e04767122ee0d504dc741d1f85154aaac
JFranklin3000 <talinthalas@msn.com>
2024-08-19 21:04:07 -0500

    Update dice.test.js

--- module/dice.test.js
+++ module/dice.test.js
@@ -151,6 +151,7 @@ describe("rollSkill", () => {
 
   test("repeated normal skill roll", async () => {
     rollDialog.getSkillRollOptions.mockReturnValue({
+      canCritD2: false,
       edge: false,
       snag: false,
       shiftUp: 0,
