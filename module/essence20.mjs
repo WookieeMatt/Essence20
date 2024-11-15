02c67bf4b69cc4a0ef2310b303ee67712df7a59b
JFranklin3000 <talinthalas@msn.com>
2024-11-14 18:42:56 -0600

    table fixes

--- module/essence20.mjs
+++ module/essence20.mjs
@@ -161,6 +161,13 @@ Handlebars.registerHelper('itemsContainType', function (items, type, options) {
   return options.inverse(this);
 });
 
+Handlebars.registerHelper('assign', function (varName, varValue, options) {
+  if (!options.data.root) {
+      options.data.root = {};
+  }
+  options.data.root[varName] = varValue;
+});
+
 /* -------------------------------------------- */
 /*  Misc Hooks                                  */
 /* -------------------------------------------- */
