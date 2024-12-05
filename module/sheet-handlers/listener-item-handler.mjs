11d088df2303777c7df57a1385f539fb8708af9f
JFranklin3000 <talinthalas@msn.com>
2024-12-04 18:25:26 -0600

    Update listener-item-handler.mjs

--- module/sheet-handlers/listener-item-handler.mjs
+++ module/sheet-handlers/listener-item-handler.mjs
@@ -294,7 +294,6 @@ export async function onShieldEquipToggle(event, actorSheet) {
  * Handles setting the values of the shield that were selected from the prompt
  * @param {Actor} actor The actor that owns the shield
  * @param {Item} currentShield The shield that we are setting values from
- * @param {Object} shieldState The location where we are getting the data from on the shield
  * @param {String} stateString The state the shield is going to
  */
 async function shieldUpdate(actor, currentShield, stateString) {
