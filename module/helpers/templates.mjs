b7696e5fd04945a689fa4869d9eb035f2761c579
JFranklin3000 <talinthalas@msn.com>
2024-12-13 15:05:13 -0600

    PR fixes

--- module/helpers/templates.mjs
+++ module/helpers/templates.mjs
@@ -94,7 +94,7 @@ export const preloadHandlebarsTemplates = async function () {
     "systems/essence20/templates/actor/parts/items/weapon/weapon-effects.hbs",
 
     //App Partials
-    "systems/essence20/templates/app/parts/sheet-field.hbs",
+    "systems/essence20/templates/app/parts/field.hbs",
 
     // Item partials.
     "systems/essence20/templates/item/parts/active-effects.hbs",
