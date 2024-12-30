52e9e22fa2ec860aa07cb8d0b6d40e9e9cb94114
JFranklin3000 <talinthalas@msn.com>
2024-12-29 20:08:00 -0600

    Update application.mjs

--- module/helpers/application.mjs
+++ module/helpers/application.mjs
@@ -1,3 +1,8 @@
+/**
+ * This function pulls the returned data from the form submission out and returns it.
+ * @param {object} formData the data returned from the form
+ * @returns the selected data from the form
+ */
 export function getFormData(formData){
   for (const [, value] of Object.entries(formData)) {
     if(value) {
