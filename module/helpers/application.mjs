ef95d3f7dbda6068768a9b5930ebdca631af8436
JFranklin3000 <talinthalas@msn.com>
2025-01-02 17:25:39 -0600

    Update application.mjs

--- module/helpers/application.mjs
+++ module/helpers/application.mjs
@@ -4,6 +4,7 @@
  * @returns the selected data from the form
  */
 export function getFormData(formData){
+  let returnData = null;
   for (const [, value] of Object.entries(formData)) {
     if(value) {
       returnData = value;
