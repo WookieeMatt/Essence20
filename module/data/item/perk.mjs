dcc28156e68870b45a15067a76f1895c72062bdb
JFranklin3000 <71140111+JFranklin3000@users.noreply.github.com>
2025-05-09 20:07:02 -0500

    Multi selection perks (#867)
    In this PR
    Ability to add perks that let you pick more than one perk from a list of perks.
    Testing
    Validate Perk selections work and require you to select the right amount.

--- module/data/item/perk.mjs
+++ module/data/item/perk.mjs
@@ -1,6 +1,6 @@
 import { E20 } from "../../helpers/config.mjs";
 
-import { makeBool, makeInt, makeStr, makeStrWithChoices } from "../generic-makers.mjs";
+import { makeBool, makeInt, makeStr, makeStrWithChoices, makeStrArrayWithChoices } from "../generic-makers.mjs";
 
 import { item } from './templates/item.mjs';
 import { itemDescription } from './templates/item-description.mjs';
@@ -17,9 +17,12 @@ export class PerkItemData extends foundry.abstract.TypeDataModel {
       choiceType: makeStrWithChoices(Object.keys(E20.perkChoiceTypes), 'none'),
       isRoleVariant: makeBool(false),
       hasMorphedToughnessBonus: makeBool(false),
+      numChoices : makeInt(1),
       prerequisite: makeStr(null),
       selectionLimit: makeInt(1),
       type: makeStrWithChoices(Object.keys(E20.perkTypes), 'general'),
+      value: makeInt(0),
+      valueLevels: makeStrArrayWithChoices(Object.keys(E20.actorLevels)),
       version: makeStrWithChoices(Object.keys(E20.gameVersions), 'powerRangers'),
     };
   }
