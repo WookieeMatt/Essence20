1e2023dac09117d5684ab54be1d03d0409ac4400
JFranklin3000 <71140111+JFranklin3000@users.noreply.github.com>
2025-12-16 18:06:59 -0600

    Bug fixes (#935)
    In this PR
    fixed issue where parent item wasn't defined for upgrades
    added info button to role perks

--- module/documents/item.mjs
+++ module/documents/item.mjs
@@ -56,7 +56,7 @@ export class Essence20Item extends Item {
 
     // Update the entry on the parent if this is a child Item
     if (['weaponEffect', 'upgrade'].includes(this.type)) {
-      const parentId = this.flags.essence20.parentId;
+      const parentId = this.flags.essence20?.parentId;
       const parentItem = this.actor.items.get(parentId);
       const key = this.flags.essence20.collectionId;
 
