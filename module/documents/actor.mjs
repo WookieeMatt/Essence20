550b088cc583d5871e5908e12af7727ed6fe3667
JFranklin3000 <71140111+JFranklin3000@users.noreply.github.com>
2026-05-10 07:02:17 -0500

    Zord Data Model Updates (#940)
    In this PR
    Changes to Zord DataModel to support base settings
    Added PreCreate to add default Zord Features
    Testing
    Validate Zord Creation works

--- module/documents/actor.mjs
+++ module/documents/actor.mjs
@@ -25,6 +25,32 @@ export class Essence20Actor extends Actor {
   }
 
   /** @override */
+  async _preCreate(data, options, user) {
+    await super._preCreate(data, options, user);
+    const CALL_TO_ACTION_ID = "Compendium.essence20.pr_crb.Item.yjhd6FRLJOsOQqN4";
+    const RECALL_FOR_REPAIRS_ID = "Compendium.essence20.pr_crb.Item.r1S0Sc4oq8axDL6C";
+
+    if (this.type == 'zord') {
+      const newItems = [];
+      const callToActionPerkData = await fromUuid(CALL_TO_ACTION_ID);
+      const recallForRepairsPerkData = await fromUuid(RECALL_FOR_REPAIRS_ID);
+      newItems.push({
+        name: callToActionPerkData.name,
+        type: callToActionPerkData.type,
+        img: callToActionPerkData.img,
+        system: callToActionPerkData.system,
+      });
+      newItems.push({
+        name: recallForRepairsPerkData.name,
+        type: recallForRepairsPerkData.type,
+        img: recallForRepairsPerkData.img,
+        system: recallForRepairsPerkData.system,
+      });
+      this.updateSource({ items: newItems });
+    }
+  }
+
+  /** @override */
   async _preUpdate(changed, options, user) {
     await super._preUpdate(changed, options, user);
 
