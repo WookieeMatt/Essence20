c3273eea8045fc33833c333bdf72b2c264984b27
JFranklin3000 <71140111+JFranklin3000@users.noreply.github.com>
2026-06-08 22:50:30 -0500

    Updates for item sheets.

--- module/essence20.mjs
+++ module/essence20.mjs
@@ -215,6 +215,25 @@ Handlebars.registerHelper(
       .format(unformattedList);
   },
 );
+
+Handlebars.registerHelper('switch', function(value, options) {
+  this.switch_value = value;
+  this.switch_break = false;
+  return options.fn(this);
+});
+
+Handlebars.registerHelper('case', function(value, options) {
+  if (value == this.switch_value) {
+    this.switch_break = true;
+    return options.fn(this);
+  }
+});
+
+Handlebars.registerHelper('default', function(value, options) {
+  if (!this.switch_break) {
+    return value;
+  }
+});
 //#endregion
 
 /* -------------------------------------------- */
