056be717b1410734b2f0e9ad7e4e214f46c1c95c
JFranklin3000 <71140111+JFranklin3000@users.noreply.github.com>
2026-06-11 22:36:15 -0500

    updates

--- module/helpers/traits.mjs
+++ module/helpers/traits.mjs
@@ -6,9 +6,15 @@ import { TraitSelector } from "../apps/trait-selector.mjs";
  * @param {Actor|Item} owner      The owning document which manages this effect
  * @private
  */
-export function onManageSelectTrait(event, owner) {
+export function onManageSelectTrait(event, owner,target=null) {
   event.preventDefault();
-  const a = event.currentTarget;
+  let a=null;
+  if (target) {
+    a = target;
+  }else {
+    a = event.currentTarget;
+  }
+
   const options = {
     name: a.dataset.target,
     title: a.parentElement.innerText,
--- module/sheets/item-sheet.mjs
+++ module/sheets/item-sheet.mjs
@@ -6,6 +6,30 @@ import { onManageSelectTrait } from "../helpers/traits.mjs";
 import { updateRoleCache } from "../helpers/utils.mjs";
 import { setEntryAndAddItem } from "../sheet-handlers/attachment-handler.mjs";
 
+  /**
+   * Handles opening the item sheet of an attached item from the info button
+   * @param {Event} data The data from the click event
+   */
+  async function _onObjectInfo(target) {
+    console.log(target.dataset)
+    const item = await fromUuid(target.dataset.uuid);
+    if (item) {
+      item.sheet.render(true);
+    }
+  }
+
+   /**
+  * Handle deleting of a Ids from an item Sheet
+  * @param {String} cssClass           Where the deleted item is on the sheet
+  * @param {DeleteEvent} event         The concluding DragEvent which contains drop data
+  * @private
+  */
+  async function _onObjectDelete(data, item) {
+    const id = data.itemKey;
+    const updateString = `system.items.-=${id}`;
+    await item.document.update({[updateString]: null});
+  }
+
 /**
  * Extend the basic ItemSheet with some very simple modifications
  * @extends {ItemSheet}
@@ -13,25 +37,12 @@ import { setEntryAndAddItem } from "../sheet-handlers/attachment-handler.mjs";
 export class Essence20ItemSheet extends HandlebarsApplicationMixin(DocumentSheetV2) {
 
   /** @override */
-  async activateEditor(name, options={}, initialContent="") {
-    options.relativeLinks = true;
-    options.plugins = {
-      menu: ProseMirror.ProseMirrorMenu.build(ProseMirror.defaultSchema, {
-        compact: true,
-        destroyOnSave: true,
-        onSave: () => {
-          this.saveEditor(name, { remove: true });
-          this.editingDescriptionTarget = null;
-        },
-      }),
-    };
-    return super.activateEditor(name, options, initialContent);
-  }
-
-
-  /** @override */
   static DEFAULT_OPTIONS = {
-    id: "item-sheet",
+    actions: {
+      deleteItem: this.#deleteItem,
+      traitSelector: this.#traitSelector,
+      viewItem: this.#viewItem,
+    },
     classes: ["essence20", "sheet", "item", "window-app"],
     tag: 'form',
     position: {
@@ -82,7 +93,6 @@ export class Essence20ItemSheet extends HandlebarsApplicationMixin(DocumentSheet
   }
 
   /* -------------------------------------------- */
-
   /** @override */
   async _prepareContext(options) {
     // Retrieve base data structure.
@@ -117,6 +127,17 @@ export class Essence20ItemSheet extends HandlebarsApplicationMixin(DocumentSheet
   }
 
   /* -------------------------------------------- */
+  static async #deleteItem(event,target) {
+    _onObjectDelete(target.dataset,this)
+  }
+
+  static async #traitSelector(event, target) {
+    onManageSelectTrait(event, this.document, target);
+  }
+
+  static async #viewItem(event,target){
+    _onObjectInfo(target);
+  }
 
   /** @override */
   activateListeners(html) {
@@ -128,52 +149,24 @@ export class Essence20ItemSheet extends HandlebarsApplicationMixin(DocumentSheet
       onManageActiveEffect(ev, this.item);
     });
 
-    html.find(".trait-selector").click(ev => {
-      onManageSelectTrait(ev, this.item);
-    });
-
-    this.form.ondrop = (event) => this._onDrop(event);
+     this.form.ondrop = (event) => this._onDrop(event);
     // Delete Effects from Weapons
-    html.find('.weaponEffect-delete').click(this._onObjectDelete.bind(this, ".weaponEffect"));
 
-    // Delete Faction from Role
-    html.find('.faction-delete').click(this._onObjectDelete.bind(this, ".faction"));
+  }
 
-    // Delete Origin Upgrade from item
-    html.find('.upgrade-delete').click(this._onObjectDelete.bind(this, ".upgrade"));
-
-    // Delete Hang Up from Influence
-    html.find('.hangUp-delete').click(this._onObjectDelete.bind(this, ".hangUp"));
-
-    //Delete a Perk off an item
-    html.find('.perk-delete').click(this._onObjectDelete.bind(this, ".perk"));
-
-    // Delete Role from Focus
-    html.find('.role-delete').click(this._onObjectDelete.bind(this, ".role"));
-
-    // Delete Role Points from Role
-    html.find('.rolePoints-delete').click(this._onObjectDelete.bind(this, ".rolePoints"));
-
-    //Delete AltMode From Origin
-    html.find('.altMode-delete').click(this._onObjectDelete.bind(this, ".altMode"));
-
-    //Delete Armor from Equipment Packages
-    html.find('.armor-delete').click(this._onObjectDelete.bind(this, ".armor"));
-
-    //Delete Shield from Equipment Packages
-    html.find('.shield-delete').click(this._onObjectDelete.bind(this, ".shield"));
-
-    //Delete Weapons from Equipment Packages
-    html.find('.weapon-delete').click(this._onObjectDelete.bind(this, ".weapon"));
-
-    //Delete Gear from Equipment Packages
-    html.find('.gear-delete').click(this._onObjectDelete.bind(this, ".gear"));
-
-    //Open Attached Item Sheet
-    html.find('.view-info').click(this._onObjectInfo.bind(this));
-
-    //Copy to clipboard
-    html.find('.clipboard-copy').click(this._onCopyClipboard.bind(this));
+   async activateEditor(name, options={}, initialContent="") {
+    options.relativeLinks = true;
+    options.plugins = {
+      menu: ProseMirror.ProseMirrorMenu.build(ProseMirror.defaultSchema, {
+        compact: true,
+        destroyOnSave: true,
+        onSave: () => {
+          this.saveEditor(name, { remove: true });
+          this.editingDescriptionTarget = null;
+        },
+      }),
+    };
+    return super.activateEditor(name, options, initialContent);
   }
 
   /**
@@ -192,46 +185,6 @@ export class Essence20ItemSheet extends HandlebarsApplicationMixin(DocumentSheet
     this.render(true);
   }
 
-  /**
-  * Handle deleting of a Ids from an item Sheet
-  * @param {String} cssClass           Where the deleted item is on the sheet
-  * @param {DeleteEvent} event         The concluding DragEvent which contains drop data
-  * @private
-  */
-  async _onObjectDelete(cssClass, event) {
-    const li = $(event.currentTarget).parents(cssClass);
-    const id = li.data("itemKey");
-    const updateString = `system.items.-=${id}`;
-
-    await this.item.update({[updateString]: null});
-    const newData = await fromUuid(this.item.uuid);
-
-    this.object.system = newData.system;
-    li.slideUp(200, () => this.render(false));
-  }
-
-  /**
-   * Handles opening the item sheet of an attached item from the info button
-   * @param {Event} data The data from the click event
-   */
-  async _onObjectInfo(data) {
-    const item = await fromUuid(data.currentTarget.dataset.uuid);
-    if (item) {
-      item.sheet.render(true);
-    }
-  }
-
-  /**
-   * Handles copying data to the clipboard
-   * @param {Event} data The data from the click event
-   */
-  _onCopyClipboard(data) {
-    const clipText = data.currentTarget.dataset.clipboard;
-    if (clipText) {
-      game.clipboard.copyPlainText(clipText);
-      ui.notifications.info(game.i18n.format("E20.ClipboardCopy", { clipText }));
-    }
-  }
 }
 
 /**
